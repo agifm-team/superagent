14bf055b9c1500a35b2606689c0c5e049a79f256
f1delius <tagazeem@gmail.com>
2024-02-02 23:11:46 +0530

    update migration

---
+++ libs/superagent/prisma/migrations/20240201181917_llm_provider_test/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `provider` on the `Agent` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Agent" DROP COLUMN "provider";
