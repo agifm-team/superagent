dabba8465ca53871bc922530a93c11fe5055bebd
f1delius <tagazeem@gmail.com>
2024-03-30 14:59:45 +0530

    remove heads up text

--- libs/ui/app/page.tsx
+++ libs/ui/app/page.tsx
@@ -117,13 +117,6 @@ export default function IndexPage() {
   return (
     <section className="container flex h-screen max-w-md flex-col justify-center space-y-8">
       <Logo width={50} height={50} />
-      <Alert>
-        <AlertTitle>Heads up!</AlertTitle>
-        <AlertDescription>
-          Use the authentication method you used the first time you signed up,
-          either email or Github. Using both will result in duplicate accounts.
-        </AlertDescription>
-      </Alert>
       <div className="flex flex-col space-y-0">
         <p className="text-lg font-bold">Logging in to Superagent</p>
       </div>
